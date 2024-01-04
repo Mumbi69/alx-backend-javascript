e4c580222ee8868dcfcf45258712825b026c81f2
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-04 13:18:52 +0300

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse === true) {
-  return [task, task2];
+    return [task, task2];
   }
 
   return [task, task2];
