7b84c1da546cdc645b9d8e02fd007a11da919f78
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-05 22:18:24 +0300

    promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,29 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
-
-    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
-
-    return [
-      {
-        status: userResult.status,
-        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason.message,
-      },
-      {
-        status: photoResult.status,
-        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason.message,
-      },
-    ];
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error.message },
-      { status: 'rejected', value: error.message },
-    ];
-  }
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const array = [];
+      for (const result of results) {
+        if (result.status === 'fulfilled') array.push({ status: result.status, value: result.value });
+        else array.push({ status: result.status, value: `${result.reason}` });
+      }
+      return array;
+    });
 }
-
-export default handleProfileSignup;
