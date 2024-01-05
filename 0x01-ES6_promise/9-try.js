6fc880be4f5445c4ca78d5d0ba1d506f068ba5c7
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-05 22:46:39 +0300

    promises

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,10 +2,9 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     queue.push(mathFunction());
-  } catch (error) {
-    queue.push(`${error}`);
-  } finally {
-    queue.push('Guardrail was processed');
+  } catch (e) {
+    queue.push(e.toString());
   }
+  queue.push('Guardrail was processed');
   return queue;
 }
