e0de429b401d6e9af68dc8484b9e7c257c784acb
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-10 00:53:55 +0300

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,15 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
   return studentList
-    .filter(student => student.location === city)
-    .map(student => {
-      const matchedGrade = newGrades.find(grade => grade.studentId === student.id);
-
-      if (matchedGrade) {
-        return {
-          ...student,
-          grade: matchedGrade.grade,
-        };
-      } else {
-        return {
-          ...student,
-          grade: 'N/A',
-        };
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const dummy = student;
+      dummy.grade = 'N/A';
+      for (const grade of newGrades) {
+        if (grade.studentId === dummy.id) {
+          dummy.grade = grade.grade;
+          break;
+        }
       }
+      return dummy;
     });
 }
