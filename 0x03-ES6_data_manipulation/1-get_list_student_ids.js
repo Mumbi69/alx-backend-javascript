9478c56f2ad57df07b593225131c386466106dd5
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-10 00:38:09 +0300

    more mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,8 @@
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
+export default function getListStudentIds(arrayOfStudents) {
+  if (!Array.isArray(arrayOfStudents)) {
+    return [];
+  }
+
+  return arrayOfStudents.map(student => student.id);
 }
+
