d7a77986b44a7682656c8b9dab1a7f0279e5af73
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-11 03:28:20 +0300

    Lets build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,38 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+class TeacherClass implements Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly fullTimeEmployee: boolean;
+  readonly yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+
+  constructor(data: {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+  }) {
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.fullTimeEmployee = data.fullTimeEmployee;
+    this.yearsOfExperience = data.yearsOfExperience;
+    this.location = data.location;
+
+    for (const key in data) {
+      if (key !== 'firstName' && key !== 'lastName' && key !== 'fullTimeEmployee' && key !== 'yearsOfExperience' && key !== 'location') {
+        this[key] = data[key];
+      }
+    }
+  }
+}
