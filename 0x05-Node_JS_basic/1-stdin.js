7d0bf5de9f4bf8dda24a10654c6fff4115964056
Mumbi69 <winfriendmumbi69@gmail.com>
2024-02-27 22:30:40 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,14 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.setEncoding('utf8');
 
-process.stdin.once('data', function(data) {
-    let name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write("This important software is now closing\n");
-    process.exit();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
