bcc052ff9380075c1dd8908e98c0ca1c5dd04367
Mumbi69 <winfriendmumbi69@gmail.com>
2024-03-05 01:29:34 +0300

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,7 +5,7 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
     it('should spy on the add function', () => {
-        const JamesBond = sinon.spy(Utils, 'calculateNumber')
+        const JohnDoe = sinon.spy(Utils, 'calculateNumber')
 
         sendPaymentRequestToApi(100, 20)
         sinon.assert.calledWith(JohnDoe, 'SUM', 100, 20)
